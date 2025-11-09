f476cb08a35b19e6a18b991133eb3f0b515aea6e
SharanRaj87 <2024tm93582@wilp.bits-pilani.ac.in>
2025-11-09 22:23:58 +0530

    Delete fsad-assignment/backend/user-authentication directory

--- fsad-assignment/backend/user-authentication/src/main/java/com/auth/repository/NotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.auth.repository;
-
-import com.auth.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-  List<Notification> findByReadFlagFalse();
-}
